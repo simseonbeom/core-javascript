ce18451dd24684374d551437db6cb2ad91b4ce6d
kindtiger <seonbeom2@gmail.com>
2024-01-22 10:52:47 +0900

    learn. javascript core
    - code structure
    - variable
    - strict mode
    - global this
    - legacy var

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,118 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+
+// var
+// let
+// const
+
+let admin = 'john' // undefined
+let name; // undefined
+
+name = 'John';
+admin = name;
+
+
+
+const ourPlanetName = 'Earth';
+const currentOnlineUserName = 'tiger';
+
+
+
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+
+
+/* variables ----------------------------------------------------------- */
+
+// - 갯수 별 상품 가격 계산하기
+// Calculate product price by quantity
+
+let calcProductPriceByQuantity;
+
+// - 구매 제품 가격의 총 합
+// Total price of purchased products
+
+let totalProductPrice;
+
+
+
+
+
+// - 구매 결제 여부
+// is... has... => true | false
+let isPayment = false;
+
+
+// let hasActive = true
+// let isClicked = true;
+
+// - 구매 결제 내역
+
+let paymentList;
+
+
+
+// - 오늘의 운세
+let fortuneOfToday = 'good';
+
+// - 상품 정보
+const productInfo = '';
+
+
+
+/* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수  
+const DAY_PER_YEAR = 365;
+
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'nike';
+
+
+// const a = 'hello';
+
+// const b = {
+//   name:'tiger',
+//   age:'35'
+// }
+
+
+let b = 'hello';
+
+const a = b;
+
+
+// const user = {
+//   name:'tiger',
+//   age:35
+// }
+
+
+// const user2 = user;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/main.js
+++ client/main.js
@@ -1,5 +1,2 @@
-const a = 10;
 
-console.log();
 
-console.log("안녕 자바스크립트!");
