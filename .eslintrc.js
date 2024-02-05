2681193f45384f8a546621c475aa9d8decdb046d
kindtiger <seonbeom2@gmail.com>
2024-02-05 15:52:27 +0900

    learn. core javascript
    - dom
    - nodeClass
    - attribute
    - manipulation
    - style
    
    - 함수 분리

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    getNode: true,
+    getNodes: true,
+    attr:true,
+    insertLast:true
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-unused-vars":"off"
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": "off",
+  },
+};
---
+++ client/chapter/dom/06.style.js
@@ -0,0 +1,82 @@
+/* -------------------- */
+/* DOM Styling          */
+/* -------------------- */
+
+
+/* 클래스를 관리할 수 있게 해주는 DOM 프로퍼티 ------------------------------------ */
+
+const first = getNode('.first');
+
+// - className – 클래스 전체를 문자열 형태로 반환해주는 프로퍼티로 클래스 전체를 관리할 때 유용
+
+console.log( first.className ); // getter
+// first.className = 'fff' // setter
+// first.className = '' // 전체 삭제
+
+// - classList – 클래스 하나를 관리할 수 있게 해주는 메서드로 개별 클래스를 조작할 때 유용
+
+
+first.classList.add('hello');
+first.classList.remove('hello');
+// first.classList.toggle('is-active')
+console.log( first.classList.contains('hello') );
+
+
+
+
+
+
+/* 스타일 변경 방법 --------------------------------------------------------- */
+
+// - style.cssText - "style" 속성 전체에 대응하므로 스타일 전체에 대한 문자열 저장
+
+
+
+first.style.cssText = `
+  display:flex;
+  margin:10px;
+  border: 1px dotted red;
+`
+
+
+first.style.background = 'orange'; // setter
+/* global getCss, setCss */
+
+
+
+console.log( getComputedStyle(first)['backgroundColor'] ); // getter
+
+
+
+
+
+
+
+// getCss('.first','fontSize') // 16px
+
+
+// setCss('.first','fontSize','30px') // setter
+
+
+
+
+// css()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 계산된 스타일 읽기 ------------------------------------------------------- */
+
+// - getComputedStyle(element, [pseudoElement]) `읽기 전용`
