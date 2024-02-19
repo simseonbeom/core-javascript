73570a9b799a612931e21361b1fc4933468cdb87
kindtiger <seonbeom2@gmail.com>
2024-02-19 14:32:11 +0900

    learn. localStorage
    - getStorage
    - setStorage
    - deleteStorage

--- client/main.js
+++ client/main.js
@@ -1,5 +1,81 @@
-const a = 10;
 
-console.log();
 
-console.log("안녕 자바스크립트!");
+
+import {
+  setStorage, 
+  getStorage, 
+  deleteStorage,
+  getNode,
+  clearContents
+} from './lib/index.js';
+
+
+
+
+// 1. textField의 값을 로컬스토리지에 저장해주세요. (setStorage)
+//     - input.addEvent... eventType: input
+
+// 2. 페이지 로딩시 바로 실행되는 함수 만들기 (IIFE)
+// 3. 스토리지값을 가져와 textField에 렌더링하기 (getStorage)
+
+const textField = getNode('#textField');
+const clear = getNode('[data-name="clear"]')
+
+
+
+const handleTextField = (e) => {
+  const value = e.currentTarget.value;
+  setStorage('text',value);
+}
+
+
+const handleClear = () => deleteStorage('text').then(clearContents(textField))
+
+
+
+textField.addEventListener('input',handleTextField);
+clear.addEventListener('click',handleClear);
+
+
+
+
+// function init(){
+//   console.log('init');
+// }
+
+
+// window.addEventListener('DOMContentLoaded',init);
+
+
+
+// IIAFE
+
+(async ()=>{
+
+
+  const data = await getStorage('text')
+
+  textField.value = data;
+
+
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
